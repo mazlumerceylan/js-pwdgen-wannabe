3b59ef4dc9fbeeee36ef21c937ef394d5b98bd6c
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-03-19 14:39:12 +0100

    consegna e passaggi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+
+Consegna: Chiedi all´utente i suoi dati personali (riportati sotto) e stampa tutto sulla pagina.
+
+1. Richiedere e salvare il nome dell´utente
+
+2. Richiedere e salvare il cognome dell´utente
+
+3.Richiedere e salvare il colore preferito dell´utente
+
+4.Richiedere e salvare il eta dell´utente
+
+5. Stampare tutto sulla pagina
+
+*/
